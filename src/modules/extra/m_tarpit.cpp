8b085622ddb1b110e4ba59516d2f82b1f4b4f1d7
Allen Day <allenday@allenday.com>
2025-12-09 17:12:24 +0000

    chore: fix tarpit metrics include path

--- src/modules/extra/m_tarpit.cpp
+++ src/modules/extra/m_tarpit.cpp
@@ -21,7 +21,7 @@
 #include "timeutils.h"
 #include "xline.h"
 #include "extension.h"
-#include "tarpit_metrics.h"
+#include "modules/tarpit_metrics.h"
 #include <array>
 #include <algorithm>
 #include <cerrno>
